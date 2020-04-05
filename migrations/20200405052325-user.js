e20f5cb3cfa9504975eb598519cd817bebf54a73
Hassain Saeed <hassain.saeed@gmail.com>
2020-04-05 02:23:55 -0400

    Initialize database, created tables for users, runner jobs, and orders

---
+++ migrations/20200405052325-user.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, callback) {
+  db.createTable('users', {
+    id: {
+      type: 'int',
+      primaryKey: true
+    },
+    email: {
+      type: 'string',
+      length: 50
+    },
+    password: {
+      type: 'string',
+      length: 100
+    },
+    first_name : {
+      type: 'string',
+      length: 20
+    },
+    last_name : {
+      type: 'string',
+      length: 20
+    },
+    created_at: {
+      type: 'datetime'
+    },
+    phone_number: {
+      type: 'string',
+      length: 15
+    }
+  }, function(err) {
+    if (err) return callback(err);
+    return callback();
+  });
+};
+
+exports.down = function(db, callback) {
+  db.dropTable('users',callback)
+};
+
+exports._meta = {
+  "version": 1
+};
---
+++ migrations/20200405054118-runner-job.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, callback) {
+  db.createTable('runner_jobs', {
+    id: {
+      type: 'int',
+      primaryKey: true
+    },
+    runners_user_id: {
+      type: 'int'
+    },
+    status: {
+      type: 'string',
+      length: 20
+    },
+    created_at: {
+      type: 'datetime'
+    },
+    ended_at : {
+      type: 'datetime'
+    },
+    is_accepting_requests: {
+      type: 'boolean'
+    },
+    store_name: {
+      type: 'string',
+      length: 100
+    },
+    runner_latitude: {
+      type: 'string',
+      length: 40
+    },
+    runner_longitude: {
+      type: 'string',
+      length: 40
+    },
+    radius: {
+      type: 'int'
+    }
+  }, function(err) {
+    if (err) return callback(err);
+    return callback();
+  });
+};
+
+exports.down = function(db, callback) {
+  db.dropTable('runner_jobs', callback)
+};
+
+exports._meta = {
+  "version": 1
+};
