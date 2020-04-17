9e336f4b42fdc4433b843b2746dff9de56707f62
Hassain Saeed <hassain.saeed@gmail.com>
2020-04-17 03:05:36 -0400

    Added functionality to return active runner jobs delivering to requesters address

--- src/api/runnerJobs.js
+++ src/api/runnerJobs.js
@@ -5,10 +5,6 @@ const runnerService = require('../services/runnerJobs');
 const middleware = require('./middleware');
 
 
-router.get('/', (req, res) => {
-  res.send('Return all the job events');
-});
-
 router.post('/', middleware.verifyToken, async (req, res, next) => {
   const userId = req.body.user_id; // could also be req.decoded.payload.userId // could also be email
   const runnerLatitude = req.body.runner_latitude;
@@ -26,5 +22,31 @@ router.post('/', middleware.verifyToken, async (req, res, next) => {
   }
 });
 
+// TODO: Update this route so it can accept optional query parameters
+// eg Status= active or status=completd, or is_accepting=1 or store_name=adonis
+router.get('/', middleware.verifyToken, async (req, res, next) => {
+  try {
+    const runnerJobs = await runnerService.getJobs();
+    console.log('💪 Returned all active Runner Jobs!');
+    return res.status(200).json({ runnerJobs });
+  } catch (err) {
+    console.log(`🔥 Error! ${err}`);
+    return next(err);
+  }
+});
+
+router.get('/nearme', middleware.verifyToken, async (req, res, next) => {
+  const requesterLatitude = req.query.requester_latitude;
+  const requesterLongitude = req.query.requester_longitude;
+
+  try {
+    const runnerJobsDeliveringToLocation = await runnerService.getJobsDeliveringToLocation(requesterLatitude, requesterLongitude);
+    console.log(('💪 Returned all active Runner Jobs delivering to the user\'s location!'));
+    return res.status(200).json({ runnerJobsDeliveringToLocation });
+  } catch (err) {
+    console.log(`🔥 Error! ${err}`);
+    return next(err);
+  }
+});
 
 module.exports = router;
--- src/services/runnerJobs.js
+++ src/services/runnerJobs.js
@@ -13,6 +13,42 @@ function dbCreateNewRunnerJob(newJobInput) {
   }));
 }
 
+// TODO: Update this function so it can accept optional parameters
+// e.g. is_accepting_requests = true
+function dbGetActiveRunnerJobs() {
+  const status = 'ACTIVE';
+  return new Promise(((resolve, reject) => {
+    mySqlConnection.query('SELECT * FROM runner_jobs WHERE status = ? ', status, (err, res) => {
+      if (err) {
+        reject(new Error(`❌ Something went wrong when querying for runner job: ${err.message}`));
+      } else {
+        // console.log(JSON.stringify(res));
+        resolve(res);
+      }
+    });
+  }));
+}
+
+// This function calculates the straight line distance (e.g as the crow flies)
+// between runner and requester and returns true if distance is less than radius
+// Taken from https://www.movable-type.co.uk/scripts/latlong.html
+// Note 'as crow flies' is different than driving distance, so we many need to change this
+function isWithinDistance(runnerLatitude, runnerLongitude, radius, requesterLatitude, requesterLongitude) {
+  const earthsRadiusinKm = 6371;
+  const deltaLatInRad = ((parseFloat(requesterLatitude) - parseFloat(runnerLatitude)) * Math.PI) / 180;
+  const deltaLongInRad = ((parseFloat(requesterLongitude) - parseFloat(runnerLongitude)) * Math.PI) / 180;
+  const runnerLatInRad = (parseFloat(runnerLatitude) * Math.PI) / 180;
+  const requesterLatInRad = (parseFloat(requesterLatitude) * Math.PI) / 180;
+  const a = Math.sin(deltaLatInRad / 2) * Math.sin(deltaLatInRad / 2)
+        + Math.cos(runnerLatInRad) * Math.cos(requesterLatInRad)
+        * Math.sin(deltaLongInRad / 2) * Math.sin(deltaLongInRad / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = earthsRadiusinKm * c;
+  console.log(`Distance is: ${distance}`);
+  if (distance < radius) return true;
+  return false;
+}
+
 
 function createJob(userId, runnerLatitude, runnerLongitude, radius, storeName) {
   console.log(' Creating a new runner job...');
@@ -35,6 +71,36 @@ function createJob(userId, runnerLatitude, runnerLongitude, radius, storeName) {
     });
 }
 
+function getJobs() {
+  console.log(' Getting all active runner jobs ...');
+  return dbGetActiveRunnerJobs()
+    .then((runnerJobs) => runnerJobs)
+    .catch((err) => {
+      throw err;
+    });
+}
+
+function getJobsDeliveringToLocation(requesterLatitude, requesterLongitude) {
+  console.log(' Getting all active runner jobs delivering to this user\'s location...');
+  return dbGetActiveRunnerJobs()
+    .then((results) => {
+      const allRunnerJobs = results;
+      let i;
+      const runnerJobsDeliveringToLocation = [];
+      for (i = 0; i < allRunnerJobs.length; i++) {
+        if (isWithinDistance(allRunnerJobs[i].runner_latitude, allRunnerJobs[i].runner_longitude, allRunnerJobs[i].radius, requesterLatitude, requesterLongitude)) {
+          runnerJobsDeliveringToLocation.push(allRunnerJobs[i]);
+        }
+      }
+      return runnerJobsDeliveringToLocation;
+    })
+    .catch((err) => {
+      throw err;
+    });
+}
+
 module.exports = {
   createJob: createJob,
+  getJobs: getJobs,
+  getJobsDeliveringToLocation: getJobsDeliveringToLocation,
 };
